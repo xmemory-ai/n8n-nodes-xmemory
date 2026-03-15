df382825e25de3a66e2d374a86b7351f19455c2d
Alexander Gusak <alexg@xmemory.ai>
2026-03-15 22:51:10 +0000

    Remove dist/

--- dist/nodes/Xmemory/Xmemory.node.d.ts
+++
@@ -1,5 +0,0 @@
-import { type IExecuteFunctions, type INodeExecutionData, type INodeType, type INodeTypeDescription } from 'n8n-workflow';
-export declare class Xmemory implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
